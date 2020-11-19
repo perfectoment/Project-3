802869fea3f421026812d16f31033026c938afb6
Lee Marvin <ryenem8@gmail.com>
2020-11-19 11:44:13 -0800

    finished question model

---
+++ models/question.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+    var Question = sequelize.define("Question", {
+      questiontext: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      description:{
+        type: DataTypes.TEXT,      
+      },
+     
+    });
+    Question.associate = function(models) {
+      Question.belongsTo(models.Quiz, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      Question.hasMany(models.Answer, {
+        onDelete: "cascade"
+      });
+    };
+  
+    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+    
+    return Question;
+  };
