a74dd1ad8c1fbfb90e21a75499f9ca9e68a260ed
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-11-28 11:27:30 -0800

    logo

--- kidzquiz/src/components/Title/index.js
+++ kidzquiz/src/components/Title/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css";
 // import Anime, { anime } from 'anime.js';
-import K from "../assets/K1.png";
-import I2 from "../assets/I2.png";
-import D from "../assets/D3.png";
-import Z4 from "../assets/Z4.png";
-import Q from "../assets/Q5.png";
-import U from "../assets/U6.png";
-import I7 from "../assets/I7.png";
-import Z8 from "../assets/Z8.png";
+import Kidz from "../assets/Kidz.png";
+// import I2 from "../assets/I2.png";
+// import D from "../assets/D3.png";
+// import Z4 from "../assets/Z4.png";
+// import Q from "../assets/Q5.png";
+// import U from "../assets/U6.png";
+// import I7 from "../assets/I7.png";
+// import Z8 from "../assets/Z8.png";
 
 
 
@@ -29,22 +29,21 @@ function Title() {
     //     autoplay: true
     // });
 
+    // play;
 
     return (
 
 
         <section class="container" id="logobox">
             <div class="row">
-                <img className="item div-thumbnail logo1" src={K} alt="K"></img>
-                <img className="item div-thumbnail logo2" src={I2} alt="I"></img>
+                <img className="item div-thumbnail logo1" src={Kidz} alt="KQ"></img>
+                {/* <img className="item div-thumbnail logo2" src={I2} alt="I"></img>
                 <img className="item div-thumbnail logo3" src={D} alt="D"></img>
                 <img className="item div-thumbnail logo4" src={Z4} alt="Z"></img>
-            {/* </div>
-            <div class="row"> */}
                 <img className="item div-thumbnail logo5" src={Q} alt="Q"></img>
                 <img className="item div-thumbnail logo6" src={U} alt="U"></img>
                 <img className="item div-thumbnail logo7" src={I7} alt="I"></img>
-                <img className="item div-thumbnail logo8" src={Z8} alt="Z"></img>
+                <img className="item div-thumbnail logo8" src={Z8} alt="Z"></img> */}
             </div>
         </section>
 
