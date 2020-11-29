74b58c68c63bd3b6eef3ded1f3e754f9c29add47
Lee Marvin <ryenem8@gmail.com>
2020-11-29 09:04:16 -0800

    added event listeners to make quiz card

--- kidzquiz/src/components/MakeQuiz/index.js
+++ kidzquiz/src/components/MakeQuiz/index.js
@@ -14,37 +14,43 @@ function MakeQuiz() {
                 <div className="card-body">
                     <h5 className="card-title">MAKE QUIZ</h5>
 
-                    <form className="form-group" ref="" onSubmit={(event) => props.handleSubmit(event)}>
+                    <form className="form-group" ref="">
 
 
-                        <label className="label">Select Subject</label>
-                        <div className="input-group-text">
-                            <input required type="text" name="subject" />
+                        <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Subject
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" value ="math" href="#" onChange={(event) => props.handleMath(event)}>Math</a>
+                            <a class="dropdown-item" value ="science" href="#" onChange={(event) => props.handleScience(event)} >Science</a>
+                            <a class="dropdown-item" value="history" href="#"onChange={(event) => props.handleHistory(event)} >History</a>
+                        </div>
                         </div>
 
                         <label className="label">Input Question</label>
                         <div className="input-group-text">
-                            <input required type="text" name="question" />
+                            <input required type="text" name="question" onChange={(event) => props.handleInputQuestion(event)} />
                         </div>
 
 
                         <label className="label">Wrong Answers</label>
                         <div className="input-group-text">
-                            <input required type="text" name="answer1" />
+                            <input required type="text" name="answer1" onChange={(event) => props.handleInputAnswer1(event)} />
                         </div>
                         <div className="input-group-text">
-                            <input required type="text" name="answer2" />
+                            <input required type="text" name="answer2" onChange={(event) => props.handleInputAnswer2(event)} />
                         </div>
                         <div className="input-group-text">
-                            <input required type="text" name="answer3" />
+                            <input required type="text" name="answer3" onChange={(event) => props.handleInputAnswer3(event)}/>
                         </div>
 
                         <label className="label">Correct Answer</label>
                         <div className="input-group-text">
-                            <input required type="text" name="correctAnswer" />
+                            <input required type="text" name="correctAnswer" onChange={(event) => props.handleInputCorrect(event)}/>
                         </div>
 
-                        <button className="btn btn-success" type="submit" />
+                        <button className="btn btn-success" type="submit" onClick={(event) => props.handleSubmit(event)}>Create Quiz</button>
 
                     </form>
                 </div>
