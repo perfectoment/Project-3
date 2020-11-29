66fe3bbda1774020a94eb941192e7c9668d1e16b
Liam Bradley <liamhbradley11@gmail.com>
2020-11-28 19:30:26 -0800

    take quiz

--- kidzquiz/src/components/TakeQuiz/index.js
+++ kidzquiz/src/components/TakeQuiz/index.js
@@ -1,62 +1,50 @@
 import React from "react";
+import React, { useState } from 'react';
 import "./style.css";
 
+export default function App() {
 
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
 
-function TakeQuiz(props) {
+    const handleAnswerOptionClick = (isCorrect) => {
+        if (isCorrect) {
+            setScore(score + 1);
+        }
 
-
-
-    
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
     return (
-
-        const [currentQuestion, setCurrentQuestion] = useState(0);
-        const [showScore, setShowScore] = useState(false);
-        const [score, setScore] = useState(0);
-    
-        const handleAnswerOptionClick = (isCorrect) => {
-            if (isCorrect) {
-                setScore(score + 1);
-            }
-    
-            const nextQuestion = currentQuestion + 1;
-            if (nextQuestion < questions.length) {
-                setCurrentQuestion(nextQuestion);
-            } else {
-                setShowScore(true);
-            }
-        };
-        return (
-            <div className='game'>
-                {showScore ? (
-                    <div className='score-section'>
-                        You scored {score} out of {questions.length}
+        <div className='app'>
+            {showScore ? (
+                <div className='score-section'>
+                    You scored {score} out of {questions.length}
+                </div>
+            ) : (
+                <>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div className='question-text'>{questions[currentQuestion].questionText}</div>
                     </div>
-                ) : (
-                    <>
-                        <div className='question-section'>
-                            <div className='question-count'>
-                                <span>Question {currentQuestion + 1}</span>/{questions.length}
-                            </div>
-                            <div className='question-text'>{questions[currentQuestion].questionText}</div>
-                        </div>
-                        <div className='answer-section'>
-                            {questions[currentQuestion].answerOptions.map((answerOption) => (
-                                <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-                            ))}
-                        </div>
-                    </>
-                )}
-            </div>
-        );
-
-
-
-    
-
-
-
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answerOption) => (
+                            <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
     );
+
+
 }
 
 export default TakeQuiz;
