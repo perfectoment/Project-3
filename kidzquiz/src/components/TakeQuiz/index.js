d487c633d9eb42308da2de07914051244a2b1b93
Liam Bradley <liamhbradley11@gmail.com>
2020-11-29 16:44:50 -0800

    updated taking quiz

--- kidzquiz/src/components/TakeQuiz/index.js
+++ kidzquiz/src/components/TakeQuiz/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
  
 
-function TakeQuiz() {
+function TakeQuiz(props) {
 
     // const [questions, setQuestions] = useState([]);
         
@@ -13,14 +13,14 @@ function TakeQuiz() {
         <div className="container">
             <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
                 <h2 className="text-2xl">
-                    This is where the question will go.
+                    {props.questiontext}
                 </h2>
             </div>
             <div className="flex flex-wrap mt-4 justify-around">
-            <button className="big-white w-2/5 p-4 text-purple-800"> Answer 1 </button>
-            <button className="big-white w-2/5 p-4 text-purple-800"> Answer 2 </button>
-            <button className="big-white w-2/5 p-4 text-purple-800"> Answer 3 </button>
-            <button className="big-white w-2/5 p-4 text-purple-800"> Answer 4 </button>
+            <button className="big-white w-2/5 p-4 text-purple-800"> {props.answertext} </button>
+            <button className="big-white w-2/5 p-4 text-purple-800"> {props.answertext} </button>
+            <button className="big-white w-2/5 p-4 text-purple-800"> {props.answertext} </button>
+            <button className="big-white w-2/5 p-4 text-purple-800"> {props.answertext} </button>
             </div>
         </div>
         
@@ -33,42 +33,3 @@ function TakeQuiz() {
 
 export default TakeQuiz;
 
-// function TakeQuiz() {
-
-//     const [questions, setQuestions] = useState([]);
-        
-//     useEffect(()=> {
-//         fetch(API).
-//         .then((res) => res.json())
-//         .then((data)=>{
-//             setQuestions(data.results);
-//         });
-//     }, []);
-
-
-//     return (
-
-
-        
-//         <div className="container">
-//             <div className="bg-white text-purple-800 p-10 rounded-lg shadow-md">
-//                 <h2 className="text-2xl">
-//                     This is where the question will go.
-//                 </h2>
-//             </div>
-//             <div className="grid cols-2 gap-6 mt-4">
-//             <button className="big-white w-2/5 p-4 text-purple-800"> {questions[0].correct_answer} </button>
-//             <button className="big-white w-2/5 p-4 text-purple-800"> {questions[0].incorrect_answers} </button>
-//             <button className="big-white w-2/5 p-4 text-purple-800"> {questions[0].incorrect_answers[1]}</button>
-//             <button className="big-white w-2/5 p-4 text-purple-800"> {questions[0].incorrect_answers[2]} </button>
-//             </div>
-//         </div>
-        
-        
-        
-        
-//     )
-
-//     };
-
-// export default TakeQuiz;
