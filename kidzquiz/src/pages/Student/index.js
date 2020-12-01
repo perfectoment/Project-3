6ce3fb5df56649111da6b5d71995e650e2bf6a62
Lee Marvin <ryenem8@gmail.com>
2020-11-30 17:30:34 -0800

    starting mapping quiz card

--- kidzquiz/src/pages/Student/index.js
+++ kidzquiz/src/pages/Student/index.js
@@ -1,9 +1,9 @@
 import Header from "../../components/Navbar";
-// import Footer from "../../components/Footer";
+import Footer from "../../components/Footer";
 // import StudentSubject from "../../components/StudentSubject";
-// import StudentQuizBank from "../../components/StudentQuizBank";
-// // import StudentQuizCard from "../../components/StudentQuizCard";
-// import Wrapper from "../../components/Wrapper";
+import StudentQuizBank from "../../components/StudentQuizBank";
+// import StudentQuizCard from "../../components/StudentQuizCard"
+import Wrapper from "../../components/Wrapper";
 // import API from "../../utils/API"
 // import Quiztaker from "../../components/QuizTaker";
 
@@ -15,33 +15,17 @@ import Header from "../../components/Navbar";
 //     return (
 
 
-//             <Wrapper>
-//                 <Header />
-//                  <StudentSubject />
-//                 {/* <StudentQuizBank >
-//                     <StudentQuizCard />
-//                 </StudentQuizBank> */}
-//                 <Footer />
-//             </Wrapper>
 
-//         <Wrapper>
-//             <Header />
-//             <div className="container">
-//                 <div className="row">
-//                     <div className="col-6">
-//                         <Quiztaker 
-//                         // id={id}
-//                         // title={title}
-//                         // subject={subject}
-//                         // questiontext={questiontext}
-//                         // answertext={answertext}
-//                         />
-//                     </div>
-//                     <div className="col-6">
-//                         <StudentQuizBank />
-//                     </div>
-//                 </div>
-//             </div>
+       <Wrapper>
+        <Header />
+           <div className="container">
+               <div className="row">
+    
+                   <div className="col-12">
+                      <StudentQuizBank />
+                  </div>
+              </div>
+           </div>
 
 
 
